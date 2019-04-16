69d3ab0ce948949c5984739a54d4797956745a9f
teddy <teddy.pangkat@gmail.com>
2019-04-16 10:28:56 +0700

    add column & row sample

--- lib/column_row_sample.dart
+++ lib/column_row_sample.dart
@@ -1,1 +1,49 @@
 import 'package:flutter/material.dart';
+
+class ColumnnRow extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Column n Row Sample')),
+        body: Column(
+
+          children: <Widget>[
+            Padding(padding: EdgeInsets.only(top: 12, bottom: 8), child: Text('Column Sample', style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),),
+            Container(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Text('Hello'),
+                  RaisedButton(child: Text('Click me!'),),
+                  Image.asset(
+                    'images/lake.jpeg',
+                    width: 70,
+                    height: 70,
+                    fit: BoxFit.fitHeight,
+                  )
+                ],
+              ),
+            ),
+            Padding(padding: EdgeInsets.only(top: 12, bottom: 8), child: Text('Row Sample', style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),),
+            Container(
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Text('Hello'),
+                  RaisedButton(child: Text('Click me!'),),
+                  Image.asset(
+                    'images/lake.jpeg',
+                    width: 70,
+                    height: 70,
+                    fit: BoxFit.fitHeight,
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
